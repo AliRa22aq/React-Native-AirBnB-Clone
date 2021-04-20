fc7cbf714233aab7dc1d6be2b5f1553475471b1c
Ali Razzaq <alirazzaquet@gmail.com>
2021-04-20 17:18:00 +0500

    first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+} from 'react-native';
+
+
+
+const App = () => {
+
+  return (
+    <SafeAreaView >
+      <StatusBar />
+     
+    </SafeAreaView>
+  );
+};
+
+export default App;
