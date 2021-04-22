1b2418f421a1ef4376933d55b91670f8e005b637
Ali Razzaq <alirazzaquet@gmail.com>
2021-04-23 02:50:56 +0500

    Map and list synchronization added

--- src/screens/SearchResultsMap/index.js
+++ src/screens/SearchResultsMap/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { View } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 import MapView from 'react-native-maps';
@@ -12,16 +12,51 @@ function SearchResultsMap() {
 
     const [selectedPlaceId, setSelectedPlaceId] = useState();
 
+    const map = useRef();
+    const flatListRef = useRef();
+    const viewConfig = useRef({itemVisiblePercentThreshold: 70})
+    const onViewChanged = useRef(({viewableItems})=> {
+        if(viewableItems.length > 0) {
+            const selectedPlace = viewableItems[0].item
+            setSelectedPlaceId(selectedPlace.id)
+        }
+    
+    }
+        
+        )
+
     const width = useWindowDimensions().width;
 
+    useEffect(() => {
+
+        if (!flatListRef || !selectedPlaceId ) {
+            return;
+        }
+
+        const index = places.findIndex(place => place.id === selectedPlaceId )
+        flatListRef.current.scrollToIndex({index})
+
+        const selectedPlace = places[index];
+        const region = {
+            latitude: selectedPlace.coordinate.latitude,
+            longitude: selectedPlace.coordinate.longitude,
+            latitudeDelta: 0.8,
+            longitudeDelta: 0.8,
+        }
+        map.current.animateToRegion(region)
+
+
+
+    }, [selectedPlaceId])
 
     return (
         <View>
             <MapView
                 style={{ width: '100%', height: '100%' }}
+                ref={map}
                 initialRegion={{
-                    latitude: 53.2734,
-                    longitude: -7.77832031,
+                    latitude: 28.4815637,
+                    longitude: -16.2291304,
                     latitudeDelta: 0.0922,
                     longitudeDelta: 0.0421,
                 }}
@@ -44,6 +79,7 @@ function SearchResultsMap() {
             }}>
 
             <FlatList 
+                ref= {flatListRef}
                 horizontal
                 snapToInterval= {width - 60}
                 snapToAlignment= "center"
@@ -51,6 +87,8 @@ function SearchResultsMap() {
                 showsHorizontalScrollIndicator = {false}
                 data={places}
                 renderItem= {({item})=> <PostCarouselItem post={item} /> } 
+                viewabilityConfig={viewConfig.current}
+                onViewableItemsChanged={onViewChanged.current}
             />
                 
             </View>
